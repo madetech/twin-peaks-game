7f23b9c92096736dc690aa84fd64d859a214a553
Luke Morton <lukemorton.dev@gmail.com>
2017-02-22 16:30:51 +0000

    Update text

--- exchanges/woodsToHotel.js
+++ exchanges/woodsToHotel.js
@@ -2,7 +2,10 @@ const extend = require('extend')
 const bed = require('./bed')
 
 module.exports = function woodsToHotel ({ character }) {
-  let question = 'You feel worn out and disappointed as you step back into the hotel. The roaring fire and chattering people around you are no comfort. The friend you made earlier is still sitting at the bar, talking to somebody else.'
+  let question = `
+    <p>You feel worn out and disappointed as you step back into the hotel.
+    <p>The roaring fire and chattering people around you are no comfort. The friend you made earlier is still sitting at the bar, talking to somebody else.
+  `
 
   return {
     question,
