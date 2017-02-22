97a34e4d798618e677c8c26850e1a366eb4bdca3
Luke Morton <lukemorton.dev@gmail.com>
2017-02-22 16:17:39 +0000

    Add some attributes

--- exchanges/diner.js
+++ exchanges/diner.js
@@ -1,12 +1,27 @@
 const hotelLobby = require('./hotelLobby')
 
 module.exports = function ({ character }) {
+  let helpLevel = ''
+
+  if (character.attributes.naivety <= 5) {
+    helpLevel = 'enough'
+  }
+
+  let devilTalk = 'For a moment you think the attendant answers in a language you cannot recognise. After looking slightly confused the attendant repeats with convincing enough instructions to get you to the hotel.'
+
+  if (character.attributes.fear > 5) {
+    devilTalk = `
+      Your struck with a sudden sense of fear. Did the attendant just speak to you in devil tongue? Paranoid, you run out of the diner and run.
+      <p>You run along the road until you notice a building that looks quite like a hotel.
+    `
+  }
+
   return {
     question: `
       <p>Walking through the diner you remember why you'd always avoided Twin Peaks in the past.
       <p>You notice a striking character out of the window before the noise of plates going into a dishwasher disturbs your thoughts.
-      <p>The diner attendent looks helpful enough so you wander over and ask for directions to the only hotel in Twin Peaks.
-      <p>For a moment you think the attendant answers in a language you cannot recognise. After looking slightly confused the attendant repeats with convincing enough instructions to get you to the hotel.
+      <p>The diner attendent looks helpful ${helpLevel} so you wander over and ask for directions to the only hotel in Twin Peaks.
+      <p>${devilTalk}
     `,
     answers: [
       {
--- exchanges/start.js
+++ exchanges/start.js
@@ -3,14 +3,30 @@ const woods = require('./woods')
 const diner = require('./diner')
 
 module.exports = function ({ character }) {
+  let halfAttribute = 'other'
+
+  if (character.attributes.lowSelfEsteem > 3) {
+    halfAttribute = 'better'
+  }
+
+  let nextSteps = 'You see a Diner nearby that looks busy enough to have some answers within.'
+
+  if (character.attributes.naivety > 5) {
+    nextSteps = 'You see a Diner nearby that looks full of lovely people who will glady help you find the hotel.'
+  } else if (character.attributes.lowSelfEsteem > 5) {
+    nextSteps = 'You see a Diner nearby. You figure your only choice is to head inside and ask someone where the hotel is.'
+  }
+
+  const question = `
+    <h1>Twin Peaks</h1>
+    <p>Finally, you think to yourself.
+    <p>The rain that slowed your journey is still pouring heavily but at least you will soon see your partner.
+    <p>You realise you have never been to Twin Peaks before, and wonder where the hotel your ${halfAttribute} half is staying at.
+    <p>${nextSteps}
+  `
+
   return {
-    question: `
-      <h1>Twin Peaks</h1>
-      <p>Finally, you think to yourself.
-      <p>The rain that slowed your journey is still pouring heavily but at least you will soon see your partner.
-      <p>You realise you have never been to Twin Peaks before, and wonder where the hotel your better half is staying at.
-      <p>You see a Diner nearby that looks busy enough to have some answers within.
-    `,
+    question,
     answers: [
       {
         text: 'Park the car, and enter the diner',
