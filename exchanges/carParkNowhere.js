313612d41eeb06d755b46379bbf93bc911b770d5
Richard Foster <richard@madetech.co.uk>
2017-02-22 16:30:00 +0000

    Improve car park nowhere

--- exchanges/carParkNowhere.js
+++ exchanges/carParkNowhere.js
@@ -3,7 +3,10 @@ const bed = require('./bed')
 module.exports = function carParkNowhere ({ character }) {
   return {
     question: `
-      <p>You remember the person in the mirror and begin to worry. You feel guilty for talking to ${character.associates.barPerson.name} as you step into the car park, into near silence. There's nothing here but the strong winds blowing around the hotel, the leaves rustling on the ground, and the darkness of the still woods. You make eye contact with the strangers who also came to look before stepping back into the hotel.
+      <p>You remember the person in the mirror and begin to worry, was it ${character.associates.partner.name}?
+      <p>Struck with a sudden sense of guilt for talking to ${character.associates.barPerson.name} instead of following them. You feel very alone as you step into the car park into near silence.
+      <p>There's nothing here but the strong winds blowing around the hotel, the leaves rustling on the ground, and the darkness of the still woods.
+      <p>You make eye contact with the strangers who also came to look, which only increases your sense of guilt, before stepping back into the hotel.
     `,
     answers: [
       {
