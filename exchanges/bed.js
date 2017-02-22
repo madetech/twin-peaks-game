d4d6d90f36debcbc70a8d90fb627cac9a67d461f
Richard Foster <richard@madetech.co.uk>
2017-02-22 15:28:46 +0000

    Remove bed/woods duplication

--- exchanges/bed.js
+++ exchanges/bed.js
@@ -1,8 +1,14 @@
 module.exports = function ({ character }) {
+  let question = `
+    You head up to your room and get into bed. As you lie in the dark, you try not to let your mind get the better of you.
+  `
+
+  if (character.decisions.foundPlayingCard) {
+    question += ' You turn over the playing card on your bedside table, inspecting it closely. You can just about make out an address on the back...'
+  }
+
   return {
-    question: `
-      You get into bed.
-    `,
+    question,
     answers: []
   }
 }
--- exchanges/woodsToHotel.js
+++ exchanges/woodsToHotel.js
@@ -1,14 +1,18 @@
 const extend = require('extend')
+const bed = require('./bed')
 
 module.exports = function woodsToHotel ({ character }) {
-  let question = 'You feel worn out and disappointed as you step back into the hotel. The roaring fire and chattering people around you are no comfort. The friend you made earlier is still sitting at the bar, talking to somebody else. You head up to your room and get into bed. As you lie in the dark, you try not to let your mind get the better of you.'
-
-  if (character.decisions.foundPlayingCard) {
-    question += ' You turn over the playing card on your bedside table, inspecting it closely. You can just about make out an address on the back...'
-  }
+  let question = 'You feel worn out and disappointed as you step back into the hotel. The roaring fire and chattering people around you are no comfort. The friend you made earlier is still sitting at the bar, talking to somebody else.'
 
   return {
     question,
-    answers: []
+    answers: [
+      {
+        text: 'Go upstairs to bed',
+        predicate: () => true,
+        effect: character => character,
+        leadsTo: bed
+      }
+    ]
   }
 }
