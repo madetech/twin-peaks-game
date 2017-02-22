95850258583625eb55b9f873f10712244dbafedf
Luke Morton <lukemorton.dev@gmail.com>
2017-02-22 16:02:44 +0000

    Restructure text

--- exchanges/start.js
+++ exchanges/start.js
@@ -6,7 +6,8 @@ module.exports = function ({ character }) {
   return {
     question: `
       <h1>Twin Peaks</h1>
-      <p>Finally, you think to yourself. The rain that slowed your journey is still pouring heavily. At least you will soon see your partner.
+      <p>Finally, you think to yourself.
+      <p>The rain that slowed your journey is still pouring heavily but at least you will soon see your partner.
       <p>You realise you have never been to Twin Peaks before, and wonder where the hotel your better half is staying at.
       <p>You see a Diner nearby that looks busy enough to have some answers within.
     `,
