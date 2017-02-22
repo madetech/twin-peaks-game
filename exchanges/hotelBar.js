562791593e5ebcd92eea6a98c9ca095c2dda4561
Luke Morton <lukemorton.dev@gmail.com>
2017-02-22 16:52:35 +0000

    Build out hotel bar exchanges

--- exchanges/hotelBar.js
+++ exchanges/hotelBar.js
@@ -3,13 +3,32 @@ const hotelBar2 = require('./hotelBar2')
 const bed = require('./bed')
 
 module.exports = function hotelBar ({ character }) {
-  return {
-    question: `
-      <p>You head into the bar. The rich smell of pine in the air and the warmth of the fire remind you of home, but you're not sure why. You sit down and order a coffee, black as midnight on a moonless night, with a generous amount of scotch. The bartender, ${character.associates.bartender.name}, is friendly enough but you find their sense of humour a little off kilter.
+  let question = `
+    <p>You head into the bar. The rich smell of pine in the air and the warmth of the fire remind you of home, but you're not sure why.
+    <p>You sit down and order a coffee, black as midnight on a moonless night, with a generous amount of scotch.
+    <p>The bartender, ${character.associates.bartender.name}, is friendly enough but you find their sense of humour a little off kilter.
+  `
 
-      <p>A person sits next to you, introducing themselves as ${character.associates.barPerson.name}. You find them attractive, and wonder if your partner knows them, or grew up with them. You feel a little guilty but engage in conversation. You notice someone familiar in the mirror behind the bar. They're standing in the doorway and seem to be looking at you. They're gone by the time you've turned around to get a better look.
-    `,
+  if (character.attributes.lowSelfEsteem < 4) {
+    question += `
+      <p>A person sits next to you, introducing themselves as ${character.associates.barPerson.name}. You find them attractive, and wonder if your partner knows them, or grew up with them. You feel a little guilty but engage in conversation.
+    `
+  }
+
+  question += `
+    <p>You are brought out of your long and interesting conversation when you notice someone familiar in the mirror behind the bar. They're standing in the doorway and seem to be looking at you.
+    <p>They're gone by the time you've turned around to get a better look.
+  `
+
+  return {
+    question,
     answers: [
+      {
+        text: 'Continue your conversation',
+        predicate: () => character.attributes.lowSelfEsteem < 4,
+        effect: character => character,
+        leadsTo: hotelBar2
+      },
       {
         text: 'Leave the bar and follow them down the hallway',
         predicate: () => true,
@@ -21,12 +40,6 @@ module.exports = function hotelBar ({ character }) {
         predicate: () => true,
         effect: character => character,
         leadsTo: bed
-      },
-      {
-        text: 'Continue your conversation',
-        predicate: () => true,
-        effect: character => character,
-        leadsTo: hotelBar2
       },
     ]
   }
--- exchanges/hotelBar2.js
+++ exchanges/hotelBar2.js
@@ -3,8 +3,7 @@ const carParkNowhere = require('./carParkNowhere')
 module.exports = function hotelBar2 ({ character }) {
   return {
     question: `
-      <p>You decide you're probably just imagining things. It's late, you're tired. You're enjoying the conversation and continue talking.
-
+      <p>You're enjoying the conversation so you continue talking.
       <p>Minutes later you hear a scream and the sound of glass shattering outside.
     `,
     answers: [
